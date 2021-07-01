d42dab3c34b3e186b04e35d159e5a621bd1dc4f0
john082Coder <johnmoody9527@gmail.com>
2021-07-01 02:26:50 -0700

    signin homepage css fixes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,17 +10,17 @@ import Logo from "../assets/footer_logo.png";
 const Footer = (props) => {
   return (
     <>
-     <Row fluid className="d-flex justify-content-between p-2 mt-4" style={{ color: "#AB6D77", margin: '0px',     borderTop: '1px solid rgba(189, 28, 59, 0.1)' }}>
+     <Row fluid className="d-flex justify-content-around pt-3 pb-3 mt-4" style={{ color: "#AB6D77", margin: '0px',     borderTop: '1px solid rgba(189, 28, 59, 0.1)' }}>
                         
-                        <Col  className="formlabel footerClass text-center">
-                            <span className="px-1">Contact Us</span>
-                            <span className="px-2">FAQ</span>
-                            <span className="px-2">Bussiness Enquiries</span>
+                        <Col  className="formlabel footerClass">
+                            <span className="px-1" style={{fontSize: "14px"}}>Contact Us</span>
+                            <span className="px-2" style={{fontSize: "14px"}}>FAQ</span>
+                            <span className="px-2" style={{fontSize: "14px"}}>Business Enquiries</span>
                         </Col>
-                        <Col  className="formlabel footerClass text-center">2021 Kawakami Inu™</Col>
-                        <Col  className="formlabel footerClass text-center">
-                            <span className="px-1">$KAWA</span>
-                            <span className="px-1">Join Community</span>
+                        <Col  className="formlabel footerClass text-center" style={{fontSize: "14px"}}>2021 Kawakami Inu™</Col>
+                        <Col  className="formlabel footerClass text-right">
+                            <span className="px-1" style={{fontSize: "14px"}}>$KAWA</span>
+                            <span className="px-1" style={{fontSize: "14px"}}>Join Community</span>
                         </Col>
                    
                 </Row>
